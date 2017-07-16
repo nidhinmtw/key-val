d7fece723abccd8d47c1cee185faf22d02c584e1
nidhinmtw <nidhinmtw@gmail.com>
2017-07-16 23:40:22 +0530

    data modification event listeners added

--- src/fileops/index.js
+++ src/fileops/index.js
@@ -1,5 +1,30 @@
 const fileWriter = require('./fileWrite');
 const fileReader = require('./fileRead');
+const fileEventManager = require('./fileopEvents');
+let writeTimeout;
+function writeOperation(path, data) {
+    if (writeTimeout) {
+        clearTimeout(writeTimeout);
+    }
+    writeTimeout = setTimeout(() => {
+        fileWriter.writeJSON(path, data);
+    }, 1000);
+}
+
+function addData(data) {
+    writeOperation(data.path, data.data);
+}
+function deleteData(data) {
+    writeOperation(data.path, data.data);
+}
+function modifyData(data) {
+    writeOperation(data.path, data.data);
+}
+fileEventManager.addEventListener('add', addData);
+fileEventManager.addEventListener('delete', deleteData);
+fileEventManager.addEventListener('modify', modifyData);
+
+
 
 module.exports = {
     writeJSON: fileWriter.writeJSON,
